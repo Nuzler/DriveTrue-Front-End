4e9de848895ed49134d1fe2b4b26968f9c5e6b3b
Nuzler <86511139+Nuzler@users.noreply.github.com>
2025-05-08 17:21:31 +0530

    Update Cart.jsx

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -89,7 +89,7 @@ const Cart = () => {
           };
          
           console.log("Sending Payment:", payment);
-           debugger;
+          
         
           const form = document.createElement('form');
           form.method = 'POST';
