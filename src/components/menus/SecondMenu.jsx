d4b28d1944210d37453b15e9e61c831fcb974a3f
Nuzler <86511139+Nuzler@users.noreply.github.com>
2025-07-27 02:04:24 +0530

    Update SecondMenu.jsx

--- src/components/menus/SecondMenu.jsx
+++ src/components/menus/SecondMenu.jsx
@@ -73,7 +73,8 @@ const SecondMenu = () => {
       <div className='bg-gradient-to-r from-red-900 to-orange-800 w-full min-h-[10px] flex justify-center gap-3 fixed top-17 z-10    '>
 
         {category.map((category)=>
-           <div className=`flex  text-sm text-amber-50 font-mono font-bold cursor-pointer my-4 px-1 hover:text-xl ease-in-out duration-200  ${activeCategory === categoryItem.value ? 'text-yellow-300 underline' : 'text-amber-50'}`> 
+           <div className=`flex text-sm font-mono font-bold cursor-pointer my-4 px-1 hover:text-xl ease-in-out duration-200 
+        ${activeCategory === categoryItem.value ? 'text-yellow-300 underline' : 'text-amber-50'}`> 
           <h1 key={category.id } onClick={()=>categoryClick(category.value)}>{category.name}</h1>
           </div>)}
       </div>
